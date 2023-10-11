e9737c750746e82f3d55258c85289291003460ae
Doruk Cem <doruk.cem@gmail.com>
2023-10-11 10:29:57 +0300

    switched resource

--- neuron.py
+++ neuron.py
@@ -1,21 +1,22 @@
-weights = [0.1, 0.2, 0]
+import numpy as np
 
-def w_sum(a,b):
-   # this is the same as taking the dot product
-   assert(len(a) == len(b))
-   output = 0
-   for i in range(len(a)):
-      output += (a[i] * b[i])
-   return output
+def sigmoid(x):
+  # Our activation function: f(x) = 1 / (1 + e^(-x))
+   return 1 / (1 + np.exp(-x))
 
-def neuron(input, weights):
-   pred = w_sum(input,weights)
-   return pred
+class Neuron:
+   def __init__(self, weights, bias):
+      self.weights = weights
+      self.bias = bias
 
-toes = [8.5, 9.5, 9.9, 9.0]
-wlrec = [0.65, 0.8, 0.8, 0.9]
-nfans = [1.2, 1.3, 0.5, 1.0]
-input = [toes[0],wlrec[0],nfans[0]]
+   def feedforward(self, inputs):
+      # Weight inputs, add bias, then use the activation function
+      total = np.dot(self.weights, inputs) + self.bias
+      return sigmoid(total)
 
-prediction = neuron(input,weights)
-print(prediction)
+weights = np.array([0, 1]) # w1 = 0, w2 = 1
+bias = 4                   # b = 4
+n = Neuron(weights, bias)
+
+x = np.array([2, 3])       # x1 = 2, x2 = 3
+print(n.feedforward(x))
